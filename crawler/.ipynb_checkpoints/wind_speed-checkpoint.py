6da611390ecef9ac84e9baf91ae5a9d435ca25ac
Katya1803 <vuvuworkplace@gmal.com>
2024-11-28 23:46:45 +0700

    Con thieu 1 nam nua

--- crawler/.ipynb_checkpoints/wind_speed-checkpoint.py
+++ crawler/.ipynb_checkpoints/wind_speed-checkpoint.py
@@ -16,8 +16,8 @@ chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
 
-start_date = dt(2023, 1, 1)
-end_date = dt(2023, 6, 30)
+start_date = dt(2024, 1, 1)
+end_date = dt(2024, 6, 30)
 station_id = "488200"
 base_url = "https://meteologix.com/vn/observations/vietnam/wind-average-10min/{}-{}z.html"
 
@@ -27,8 +27,8 @@ urls = [
     for hour in range(24)
 ]
 
-output_file = "DS-project/wind_speed/HaNoi_wind_speed_2023.csv"
-error_log_file = "DS-project/wind_speed/failed_urls.txt"
+output_file = "wind_speed/HaNoi_wind_speed_2023.csv"
+error_log_file = "wind_speed/failed_urls.txt"
 batch_size = 100
 
 def initialize_csv():
